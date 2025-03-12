c20afd80ff20356795827f321aaac3d3d15ce6dd
James.Murrell <James.Murrell@ACCESS-BZ30S14>
2025-03-12 13:57:06 +0000

    Add project files.

---
+++ AzureDevOpsViewer/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace AzureDevOpsViewer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox cmbProducts;
+        private System.Windows.Forms.ComboBox cmbIterations;
+        private System.Windows.Forms.Button btnFetchData;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear; // Declare the Clear button
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.cmbProducts = new System.Windows.Forms.ComboBox();
+            this.cmbIterations = new System.Windows.Forms.ComboBox();
+            this.btnFetchData = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbProducts
+            // 
+            this.cmbProducts.FormattingEnabled = true;
+            this.cmbProducts.Location = new System.Drawing.Point(187, 27);
+            this.cmbProducts.Name = "cmbProducts";
+            this.cmbProducts.Size = new System.Drawing.Size(289, 21);
+            this.cmbProducts.TabIndex = 0;
+            // 
+            // cmbIterations
+            // 
+            this.cmbIterations.FormattingEnabled = true;
+            this.cmbIterations.Location = new System.Drawing.Point(187, 65);
+            this.cmbIterations.Name = "cmbIterations";
+            this.cmbIterations.Size = new System.Drawing.Size(289, 21);
+            this.cmbIterations.TabIndex = 1;
+            // 
+            // btnFetchData
+            // 
+            this.btnFetchData.Location = new System.Drawing.Point(606, 27);
+            this.btnFetchData.Name = "btnFetchData";
+            this.btnFetchData.Size = new System.Drawing.Size(75, 23);
+            this.btnFetchData.TabIndex = 2;
+            this.btnFetchData.Text = "Fetch Data";
+            this.btnFetchData.UseVisualStyleBackColor = true;
+            this.btnFetchData.Click += new System.EventHandler(this.btnFetchData_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(61, 132);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(701, 287);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Please Select Project";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select Quarter";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(606, 70);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 461);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnFetchData);
+            this.Controls.Add(this.cmbIterations);
+            this.Controls.Add(this.cmbProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        // Add this method for the Clear button
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear(); // Clears all rows
+            cmbProducts.SelectedIndex = -1; // Resets the ComboBox
+            cmbIterations.SelectedIndex = -1; // Resets the ComboBox
+        }
+    }
+}
---
+++ AzureDevOpsViewer/Form1.cs
@@ -0,0 +1,297 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AzureDevOpsViewer
+{
+    public partial class Form1 : Form
+    {
+        private string organization = "access-devops"; // Replace with your organization name
+       private string personalAccessToken = "4qZqGkQYuSxq4cwh6bdMVF4zRuNhINSP7fPLdaNkBFR0WPlCmvqlJQQJ99AKACAAAAAFWvOZAAASAZDOrWsN"; // Replace with your PAT
+
+        //4zs2uxfzk37wbo26txnt36p5d6oxuh5vldd3bp7qaxmdjswubeha
+        public Form1()
+        {
+            InitializeComponent();
+            FetchProjects();
+
+            // Wire up the event for product selection
+            cmbProducts.SelectedIndexChanged += cmbProducts_SelectedIndexChanged;
+
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells; // Adjust column size
+            dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;     // Adjust row size
+        }
+
+        private void FetchProjects()
+        {
+            var client = new RestClient($"https://dev.azure.com/{organization}/_apis/projects?api-version=6.0");
+
+            var request = new RestRequest
+            {
+                Method = Method.Get
+            };
+            request.AddHeader("Authorization", $"Basic {Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($":{personalAccessToken}"))}");
+
+            RestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                dynamic data = JsonConvert.DeserializeObject(response.Content);
+                cmbProducts.Items.Clear();
+
+                List<string> projects = new List<string>();
+                foreach (var project in data.value)
+                {
+                    projects.Add((string)project.name);
+                }
+
+                projects = projects.OrderBy(project => project).ToList();
+                cmbProducts.Items.AddRange(projects.ToArray());
+
+                if (cmbProducts.Items.Count > 0)
+                {
+                    cmbProducts.SelectedIndex = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error fetching projects: " + response.Content, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cmbProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbProducts.SelectedItem != null)
+            {
+                string selectedProject = cmbProducts.SelectedItem.ToString();
+                FetchIterations(selectedProject);
+            }
+        }
+
+        private void FetchIterations(string project)
+        {
+           var client = new RestClient($"https://dev.azure.com/{organization}/{project}/_apis/work/teamsettings/iterations?api-version=7.1");
+       
+
+
+            var request = new RestRequest
+            {
+                Method = Method.Get
+            };
+            request.AddHeader("Authorization", $"Basic {Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($":{personalAccessToken}"))}");
+
+            RestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                dynamic data = JsonConvert.DeserializeObject(response.Content);
+                cmbIterations.Items.Clear();
+
+                HashSet<string> parentIterations = new HashSet<string>();
+                foreach (var iteration in data.value)
+                {
+                    string iterationPath = (string)iteration.path;
+
+                    if (iterationPath.Contains("\\"))
+                    {
+                        string parentIteration = string.Join("\\", iterationPath.Split('\\').Take(2));
+                        if (parentIterations.Add(parentIteration))
+                        {
+                            cmbIterations.Items.Add(parentIteration);
+                        }
+                    }
+                    else
+                    {
+                        if (parentIterations.Add(iterationPath))
+                        {
+                            cmbIterations.Items.Add(iterationPath);
+                        }
+                    }
+                }
+
+                if (cmbIterations.Items.Count > 0)
+                {
+                    cmbIterations.SelectedIndex = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error fetching iterations: " + response.Content, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnFetchData_Click(object sender, EventArgs e)
+        {
+            if (cmbProducts.SelectedItem == null || cmbIterations.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a product and iteration.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string project = cmbProducts.SelectedItem.ToString();
+            string iteration = cmbIterations.SelectedItem.ToString();
+
+            FetchWorkItems(project, iteration);
+        }
+
+        private void FetchWorkItems(string project, string iteration)
+        {
+            var client = new RestClient($"https://dev.azure.com/{organization}/{project}/_apis/wit/wiql?api-version=6.0");
+
+            var request = new RestRequest
+            {
+                Method = Method.Post
+            };
+            request.AddHeader("Authorization", $"Basic {Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($":{personalAccessToken}"))}");
+
+            var wiql = new
+            {
+                query = $@"
+                Select [System.Id]
+                From WorkItems
+                Where [System.IterationPath] = '{iteration}' 
+                  AND [System.WorkItemType] = 'Epic'"
+            };
+            request.AddJsonBody(wiql);
+
+            RestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                dynamic data = JsonConvert.DeserializeObject(response.Content);
+                dataGridView.Rows.Clear();
+
+                if (dataGridView.Columns.Count == 0)
+                {
+                    dataGridView.Columns.Add("EpicID", "Epic ID");
+                    dataGridView.Columns.Add("EpicState", "Epic State");
+                    dataGridView.Columns.Add("Title", "Title");
+                    dataGridView.Columns.Add("Effort", "Effort");
+                    dataGridView.Columns.Add("StoryPoints", "Story Points");
+                    dataGridView.Columns.Add("Difference", "Effort - Story Points");
+                }
+
+                if (data.workItems != null && data.workItems.Count > 0)
+                {
+                    foreach (var item in data.workItems)
+                    {
+                        string epicId = item.id.ToString();
+                        FetchEpicDetails(project, epicId);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No Epics found in this iteration.", "No Epics", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error fetching Epics: " + response.Content, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FetchEpicDetails(string project, string epicId)
+        {
+            var client = new RestClient($"https://dev.azure.com/{organization}/{project}/_apis/wit/workitems/{epicId}?$expand=relations&api-version=6.0");
+
+            var request = new RestRequest
+            {
+                Method = Method.Get
+            };
+            request.AddHeader("Authorization", $"Basic {Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($":{personalAccessToken}"))}");
+
+            RestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                dynamic workItemDetails = JsonConvert.DeserializeObject(response.Content);
+
+                // Get the Epic State value
+                string epicState = workItemDetails.fields.ContainsKey("System.State") ? workItemDetails.fields["System.State"].ToString() : "N/A";
+
+                // Get the Epic Title
+                string title = workItemDetails.fields.ContainsKey("System.Title") ? workItemDetails.fields["System.Title"].ToString() : "N/A";
+
+                // Get the total story points from child work items
+                double totalStoryPoints = FetchChildStoryPoints(project, workItemDetails);
+
+                // Get Effort value
+                double effort = workItemDetails.fields.ContainsKey("Microsoft.VSTS.Scheduling.Effort") ? (double)workItemDetails.fields["Microsoft.VSTS.Scheduling.Effort"] : 0;
+
+                // Debug: log effort and story points
+                Console.WriteLine($"Effort: {effort}, Total Story Points: {totalStoryPoints}");
+
+                // Calculate the difference between Effort and Story Points
+                double difference = effort - totalStoryPoints;
+
+                // Debug: log the difference
+                Console.WriteLine($"Difference: {difference}");
+
+                // Add columns to DataGridView if not already done
+                if (dataGridView.Columns.Count == 0)
+                {
+                    dataGridView.Columns.Add("EpicID", "Epic ID");
+                    dataGridView.Columns.Add("EpicState", "Epic State");
+                    dataGridView.Columns.Add("Title", "Title");
+                    dataGridView.Columns.Add("Effort", "Effort");
+                    dataGridView.Columns.Add("StoryPoints", "Story Points");
+                    dataGridView.Columns.Add("Difference", "Effort - Story Points");
+                }
+                //MessageBox.Show($"Epic details for Epic ID: {epicId} fetched successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Add Epic details and the calculated difference to the DataGridView
+                dataGridView.Rows.Add(epicId, epicState, title, effort, totalStoryPoints, difference);
+
+                // Auto-resize columns after adding data
+                dataGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            else
+            {
+                MessageBox.Show($"Error fetching Epic details: {response.Content}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private double FetchChildStoryPoints(string project, dynamic epicDetails)
+        {
+            double totalStoryPoints = 0;
+
+            if (epicDetails.relations != null)
+            {
+                foreach (var relation in epicDetails.relations)
+                {
+                    if (relation.rel == "System.LinkTypes.Hierarchy-Forward")
+                    {
+                        string childUrl = relation.url.ToString();
+                        string childId = childUrl.Split('/').Last();
+
+                        var client = new RestClient($"https://dev.azure.com/{organization}/{project}/_apis/wit/workitems/{childId}?api-version=6.0");
+
+                        var request = new RestRequest
+                        {
+                            Method = Method.Get
+                        };
+                        request.AddHeader("Authorization", $"Basic {Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($":{personalAccessToken}"))}");
+
+                        RestResponse response = client.Execute(request);
+
+                        if (response.IsSuccessful)
+                        {
+                            dynamic childDetails = JsonConvert.DeserializeObject(response.Content);
+                            if (childDetails.fields.ContainsKey("Microsoft.VSTS.Scheduling.StoryPoints"))
+                            {
+                                totalStoryPoints += (double)childDetails.fields["Microsoft.VSTS.Scheduling.StoryPoints"];
+                            }
+                        }
+                    }
+                }
+            }
+
+            return totalStoryPoints;
+        }
+    }
+}
